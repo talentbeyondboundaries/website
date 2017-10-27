50e9520d5e7b509fb7a1aa9e56136c86ec2cd53d
Aleksandar Nikolic <digrejzo@gmail.com>
2017-10-27 21:12:54 +0200

    Add Gulp and accompanying plugins to automate workflow

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+var nunjucksRender = require('gulp-nunjucks-render');
+var htmlPrettify = require('gulp-html-prettify');
+var removeEmptyLines = require('gulp-remove-empty-lines');
+
+var paths = {
+	server:             'dist',
+	html_src:           'src/**/*.html',
+	html_dest:          'dist/**/*.html',
+	js_src:             'src/js/**/*.js',
+	js_dest:            'dist/js',
+	sass_src:           'src/sass/**/*.scss',
+	css_dest:           'dist/css',
+	nunjucks_views:     'src/views/**/*.+(html|nunjucks)',
+	nunjucks_templates: 'src/templates'
+}
+
+gulp.task('minifyJS', function() {
+	return gulp.src(paths.js_src)
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(plumber.stop())
+		.pipe(rename(function(path) {
+			path.basename += ".min";
+		}))
+		.pipe(gulp.dest(paths.js_dest))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+	return gulp.src(paths.sass_src)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(paths.css_dest));
+});
+
+gulp.task('autoprefix', function() {
+	return gulp.src(paths.css_dest + '/styles.css')
+		.pipe(autoprefix('last 2 versions', 'ie 9'))
+		.pipe(gulp.dest(paths.css_dest));
+});
+
+gulp.task('uglifyCSS', function() {
+	return gulp.src(paths.css_dest + '/styles.css')
+		.pipe(uglifycss())
+		.pipe(gulp.dest(paths.css_dest))
+		.pipe(browserSync.stream({match: '**/*.css'}));
+});
+
+gulp.task('compileCSS', function(callback) {
+	runSequence(
+		'sass',
+		'autoprefix',
+		'uglifyCSS',
+		callback
+	);
+});
+
+gulp.task('nunjucks', function() {
+	return gulp.src(paths.nunjucks_views)
+		.pipe(plumber())
+		.pipe(nunjucksRender({
+			path: [paths.nunjucks_templates]
+		}))
+		.pipe(htmlPrettify({indent_char: ' ', indent_size: 4}))
+		.pipe(removeEmptyLines())
+		.pipe(gulp.dest(paths.server));
+});
+
+gulp.task('serve', ['compileCSS', 'minifyJS', 'nunjucks'], function() {
+	browserSync.init({
+		server: paths.server
+	});
+	gulp.watch(paths.sass_src, ['compileCSS']);
+	gulp.watch(paths.js_src, ['minifyJS']);
+	gulp.watch(paths.html_src, ['nunjucks']);
+	gulp.watch(paths.html_dest).on('change', browserSync.reload);
+});
+
+gulp.task('build', ['compileCSS', 'minifyJS', 'nunjucks']);
+
+gulp.task('default', ['serve']);
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Gulper
+Ultimate badassery - makes life a lot easier when working with simple HTML/CSS/JS projects and static stuff. Uses [Nunjucks](https://mozilla.github.io/nunjucks/) templating engine.
+
+## Packages used
+* `gulp` - badassery itself
+* `gulp-plumber` - prevents pipe/stream from breaking when compile errors occur
+* `gulp-uglify` - JS minification
+* `gulp-uglifycss` - CSS minification
+* `gulp-sass` - Sass compiler
+* `gulp-autoprefixer` - adds CSS vendor prefixes
+* `gulp-rename` - self explanatory
+* `run-sequence` - run tasks in a sequence
+* `browser-sync` - injects CSS/JS assets when they change, syncs scroll/click events with all browsers/devices
+* `gulp-nunjucks-render` - compiles Nunjucks templates into static HTML files, kicks ass
+* `gulp-html-prettify` - makes sure indentation is consistent when Nunjucks templates are compiled
+* `gulp-remove-empty-lines` - removes empty lines created after Nunjucks templates are compiled
+
+## Setup guide
+1. Run `npm install` to install package dependencies
+2. Tweak `gulpfile.js` based on your needs or use it as-is
+3. Run `gulp serve`
+4. Voila!
+
+---
+### Todo
+* Add `gulp-rev` to do CSS/JS revisioning (and `gulp-rev-replace`) and enable cache busting
+* Add `.htaccess` from HTML5 Boilerplate for caching/gzipping
