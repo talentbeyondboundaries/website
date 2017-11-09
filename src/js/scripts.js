72c0d4057451125b6fef4f4aa5580f7a9c13a281
Aleksandar Nikolic <digrejzo@gmail.com>
2017-11-09 14:51:03 +0100

    Add Popup functionality and markup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
 		$('.offcanvas-toggle').toggleClass('offcanvas-toggle--active');
 		$('.offcanvas').toggleClass('offcanvas--active');
 	});
+
+	// Popups toggler functionality
+	$('.popup-toggle').on('click', function(e) {
+		e.preventDefault();
+		var toggler = $(this),
+			target = $(toggler.attr('data-popup-target'));
+
+		target.addClass('popup--show').find('.popup__dismiss').on('click', function(e) {
+			e.preventDefault();
+			target.removeClass('popup--show').one('transitionend', function() {
+				target.trigger('popup-close');
+			});
+		});
+	});
 });
